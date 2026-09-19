57b926341b76be88ba00faab6a05a4ad6756f136
Mara Mellstrom <mara@maramellstrom.com>
2026-09-19 16:06:12 -0400

    Delete patch.js

--- patch.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('tmdb.js', 'utf8');
-content = content.replace("  url.searchParams.set('api_key', process.env.TMDB_API_KEY);\n", '');
-content = content.replace('const res = await fetch(url.toString());', "const res = await fetch(url.toString(), { headers: { Authorization: 'Bearer ' + process.env.TMDB_API_KEY, accept: 'application/json' } });");
-fs.writeFileSync('tmdb.js', content);
-console.log('patched successfully');
